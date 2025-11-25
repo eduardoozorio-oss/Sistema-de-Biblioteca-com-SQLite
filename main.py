880357fd390e6f10098a2898a36f880e7baa5b16
eduardo furtado <eduardo.ozorio@aluno.senai.br>
2025-11-25 14:40:40 -0300

    Criar tabela no banco

--- main.py
+++ main.py
@@ -6,3 +6,21 @@ import sqlite3
 
 def conectar():
     return sqlite3.connect("biblioteca.db")
+
+# Criar tabela no banco
+def criar_tabela():
+    con = conectar()
+    cur = con.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS livros (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT NOT NULL,
+            autor TEXT NOT NULL,
+            ano INTEGER,
+            disponivel TEXT
+        )
+    """)
+
+    con.commit()
+    con.close()
